6df314d8ed6f966f4237fd22de2cc307f0a2a255
Tiago Ribeiro <tiago.astro@gmail.com>
2020-05-20 16:31:04 -0700

    Implement suggestions from code review.

--- setup.py
+++ setup.py
@@ -11,22 +11,23 @@ __all__ = ["__version__"]
 
 __version__ = "{version}"
 """
-tools_path = pathlib.PurePosixPath(setuptools.__path__[0])
-base_prefix = pathlib.PurePosixPath(sys.base_prefix)
+tools_path = pathlib.Path(setuptools.__path__[0])
+base_prefix = pathlib.Path(sys.base_prefix)
 data_files_path = tools_path.relative_to(base_prefix).parents[1]
 
 setuptools.setup(
     name="ts_watcher",
     description="",
-    use_scm_version={"write_to": "python/lsst/ts/watcher/version.py",
-                     "write_to_template": scm_version_template},
+    use_scm_version={
+        "write_to": "python/lsst/ts/watcher/version.py",
+        "write_to_template": scm_version_template,
+    },
     setup_requires=["setuptools_scm", "pytest-runner"],
     install_requires=install_requires,
     package_dir={"": "python"},
     packages=setuptools.find_namespace_packages(where="python"),
     package_data={"": ["*.rst", "*.yaml"]},
-    data_files=[(os.path.join(data_files_path, "schema"),
-                 ["schema/Watcher.yaml"])],
+    data_files=[(data_files_path / "schema", ["schema/Watcher.yaml"])],
     scripts=["bin/run_watcher.py"],
     tests_require=tests_require,
     extras_require={"dev": dev_requires},
@@ -34,5 +35,5 @@ setuptools.setup(
     project_urls={
         "Bug Tracker": "https://jira.lsstcorp.org/secure/Dashboard.jspa",
         "Source Code": "https://github.com/lsst-ts/ts_watcher",
-    }
+    },
 )
